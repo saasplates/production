86d695ee902a760c43608b7132db5620652ca255
Arjun Aditya <meow@nermalcat69.dev>
2024-12-20 01:34:00 +0530

    rah

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# site-saasplates
- 
+## GitAnalyzer
+
+GitAnalyzer is a tool that analyzes git repositories and provides insights into the codebase.
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import "../app.css";
+</script>
+
+<svelte:head>
+  <!-- Primary Meta Tags -->
+  <title>Chill Developer Analyzer | Check Your Developer Type</title>
+  <meta name="title" content="Chill Developer Analyzer" />
+  <meta name="description" content="Find out if you're a chill developer or you need to chill a little bit!" />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://chill.zerops.xyz/" />
+  <meta property="og:title" content="Chill Developer Analyzer" />
+  <meta property="og:description" content="Find out if you're a chill developer or or you need to chill a little bit!" />
+  <meta property="og:image" content="https://chill.zerops.xyz/og.png" />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://chill.zerops.xyz/" />
+  <meta property="twitter:title" content="Chill Developer Analyzer" />
+  <meta property="twitter:description" content="Find out if you're a chill developer or or you need to chill a little bit!" />
+  <meta property="twitter:image" content="https://chill.zerops.xyz/og.png" />
+</svelte:head>
+
+<slot />
