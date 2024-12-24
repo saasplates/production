e478b022f177a045410af909a9e2786cad8cd234
Arjun Aditya <meow@nermalcat69.dev>
2024-12-24 23:52:48 +0530

    Update LargeAdvertiseCard.svelte

--- src/lib/components/LargeAdvertiseCard.svelte
+++ src/lib/components/LargeAdvertiseCard.svelte
@@ -21,7 +21,6 @@
       src={image} 
       draggable="false"
       loading="lazy"
-      loading="lazy"
       alt={title}
       class="w-full h-[200px] object-cover transition-transform duration-500" 
     />
