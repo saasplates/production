48d4b68647a4cd39986e99ec762e2f4aec44fb53
Arjun Aditya <meow@nermalcat69.dev>
2024-12-28 01:31:30 +0530

    iterate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -180,10 +180,10 @@
 			<div class="flex justify-center mb-5">
 				<a target="_blank" href="https://www.producthunt.com/posts/saasplates?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-saasplates"><img draggable="false" src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=735284&theme=light" alt="SaasPlates - Premium&#0032;Open&#0032;Source&#0032;&#0038;&#0032;Paid&#0032;Boilerplates | Product Hunt" style="width: 200px; height: 43px;" width="250" height="54" /></a>
 			</div>
-			<h1 class="text-4xl font-bold text-gray-900 sm:text-5xl md:text-6xl">
+			<h1 class="text-4xl font-bold text-gray-900 sm:text-6xl">
 				Find & Ship your SaaS Faster.
 			</h1>
-			<p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+			<p class="pt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
 				Browse a curated library of SaaS boilerplates and micro-starters. From complete solutions to specific functionality starters, everything you need is here.
 			</p>
 		</div>
